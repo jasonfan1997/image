7cc4fc5590c3669ae7429e3fb7ba87a168c25889
King-of-Infinite-Space <fcbarcelona.plh@gmail.com>
2017-09-22 12:28:39 +0800

    Should be working for both arch

--- keras_top.py
+++ keras_top.py
@@ -6,12 +6,15 @@ from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
 from keras.metrics import top_k_categorical_accuracy, sparse_top_k_categorical_accuracy
 from keras import applications
+
 import keras
 import numpy as np
 import tensorflow as tf
 
 
 top_model_weights_path = 'top_model_res50_365.h5'
+train_data_dir = 'bottleneck_features_train.npy'
+validation_data_dir = 'bottleneck_features_validation.npy'
 train_labels = np.load('training_labels.npy')
 validation_labels = np.load('validation_labels.npy')
 nb_train_samples = len(train_labels)
@@ -24,31 +27,41 @@ def to_one_hot(array):
     n_values = np.max(array) + 1
     return np.eye(n_values)[array]
 
+def top_3_categorical_accuracy(y_true, y_pred, k=3):
+    return K.mean(K.in_top_k(y_pred, K.argmax(y_true, axis=-1), k))
+
+
+def shuffle_data(data, label):
+    assert len(data) == len(label)
+    p = np.random.permutation(len(data))
+    return data[p], label[p]
 
 def train_top_model():
     np.random.seed(1)
     global train_labels
     global validation_labels
-    train_data = np.load('bottleneck_features_train.npy')
+    train_data = np.load(train_data_dir)
     #train_labels = np.array([0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
     #train_labels have been defined  globally
     
-    validation_data = np.load('bottleneck_features_validation.npy')
+    validation_data = np.load(validation_data_dir)
     #validation_labels = np.array([0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
     # array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
     model = Sequential()
     #model.add(Flatten(input_shape=train_data.shape[1:]))
-    model.add(Dropout(0.3, input_shape=(1536,)))
-    model.add(Dense(1000, activation='relu'))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.2, input_shape=(train_data.shape[1],)))
+    #model.add(Dense(1000, activation='relu'))
+    #model.add(Dropout(0.2))
     model.add(Dense(80, activation='softmax'))
     
+    model.summary()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy',top_3_categorical_accuracy])
     #keras.optimizers.RMSprop(lr=0.0005)
-    train_labels=train_labels.reshape((len(train_labels),1))
+    
+    #shuffle will be done during fitting
+    '''train_labels=train_labels.reshape((len(train_labels),1))
     trainmerge=np.append(train_data,train_labels,axis=1)
     validation_labels=validation_labels.reshape((len(validation_labels),1))
-    
     validation_data=validation_data[:-20,:]
     validationmerge=np.append(validation_data,validation_labels,axis=1)
     np.random.shuffle(trainmerge)
@@ -56,13 +69,14 @@ def train_top_model():
     train_data=trainmerge[:,:-1]
     train_labels=trainmerge[:,-1]
     validation_data=validationmerge[:,:-1]
-    validation_labels=validationmerge[:,-1]  
+    validation_labels=validationmerge[:,-1]'''  
     #validation_labels=K.one_hot(validation_labels,80)
     validation_labels=to_one_hot(validation_labels.flatten())
     train_labels=to_one_hot(train_labels.flatten())
     model.load_weights(top_model_weights_path)
     model.fit(train_data, train_labels,
               epochs=epochs,
+              shuffle=True,
               batch_size=batch_size,
               validation_data=(validation_data, validation_labels))
     model.save_weights(top_model_weights_path)
