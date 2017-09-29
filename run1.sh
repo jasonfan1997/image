86173a5fd7d28e69a5e37668b9e57303716c6dd4
King-of-Infinite-Space <fcbarcelona.plh@gmail.com>
2017-09-29 17:54:13 +0800

    added sh

---
+++ run1.sh
@@ -0,0 +1,2 @@
+python train_placesCNN.py --epoch 20 --batch-size 200 --print-freq 20
+#train the fc layer only
---
+++ run2.sh
@@ -0,0 +1,2 @@
+python train_placesCNN.py --epoch 50 --batch-size 200 --resume 'checkpoint_latest.pth.tar' --mode 2 --print-freq 20
+#train the fc layer only
